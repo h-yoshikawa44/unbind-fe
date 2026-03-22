9c1eebe7c7a424607c28bb4f1d15bc2828b6fc56
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2026-03-22 17:43:13 +0900

    settings: lint, format系のコマンド整備 + fix(#2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
-import { defineConfig } from "vite-plus";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite-plus';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  lint: { options: { typeAware: true, typeCheck: true } },
+  lint: {
+    options: { typeAware: true, typeCheck: true },
+    plugins: ['react', 'typescript'],
+    categories: {
+      correctness: 'error',
+      suspicious: 'warn',
+    },
+    env: {
+      browser: true,
+      es2020: true,
+    },
+    rules: {
+      // 明示的な React インポートは不要なのでオフ
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  fmt: {
+    singleQuote: true,
+  },
   plugins: [react()],
 });
